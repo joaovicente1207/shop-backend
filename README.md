c36ccf19857c6d2b890204d03b4f468795a59abd
João Vicente <joaovicente1207@gmail.com>
2023-02-22 23:35:49 -0400

    add dockerfile

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # backend from shop-app
 
 ## Shop App
+
+To run this project, install docker and run these follow commands:
+
+sudo docker build . -f Dockerfile.dev -t shop-app-backend
+sudo docker run -dp 3080:3080 shop-app-backend
--- server.js
+++ server.js
@@ -3,25 +3,17 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
 const routes = require("./src/routes");
-
 const startDB = require("./db.js")
-
-const PORT = process.env.PORT || 5000
-const app = express();
 
 startDB();
 
+const app = express();
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(routes);
 
-const soma = (param1,param2) =>  {
-    return param1+param2
-}
-
-
-
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
